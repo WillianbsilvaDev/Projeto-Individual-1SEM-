7ec9e7adc76278736b265aeecfb5f00b3c9374d9
WillianbsilvaDev <willian.dsilva@sptech.school>
2025-05-19 01:40:41 -0300

    criação do index e aba canais

--- Portal FutebolSP Site/src/database/script-tabelas.sql
+++ Portal FutebolSP Site/src/database/script-tabelas.sql
@@ -8,15 +8,28 @@ email varchar(45) not null,
 senha varchar(45) not null
 )auto_increment = 1;
 
+
+
 create table canais(
 id int primary key,
 nomeCanal int
 );
 
-create table cadrastro_Canais(
-fkcadastroUser int,
+create table usuario_canais(
+fkUsuario int,
 fkCanais int,
-primary key (fkcadastroUser,fkCanais),
-constraint cadastroUserFK foreign key (fkcadastroUser) references cadastroUser(id),
+inscrito boolean default false,
+primary key (fkUsuario,fkCanais),
+constraint usuarioFK foreign key (fkUsuario) references usuario(idUsuario),
 constraint canaisFK foreign key (fkCanais) references canais(id)
 );
+
+create table anotacoes(
+id int auto_increment primary key,
+assunto varchar(30),
+texto varchar(200),
+fkUsuario int,
+fkCanais int,
+constraint chat_fkusuario foreign key (fkUsuario) references usuario_canais(fkUsuario),
+constraint chat_fkcanais foreign key (fkCanais) references usuario_canais(fkCanais)
+) auto_increment = 1;
