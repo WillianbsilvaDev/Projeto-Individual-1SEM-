78cf09baed9f64698471b3d90dc7be67035d979a
willianbsilvadev <willian.dsilva@sptech.school>
2025-06-01 22:56:37 -0300

    att

--- Portal FutebolSP Site/src/models/graficoModel.js
+++ Portal FutebolSP Site/src/models/graficoModel.js
@@ -1,16 +1,16 @@
-const database = require("../database");
+var database = require("../database/config")
 
 function contarInscricoes() {
   const query = `
     SELECT c.nomeCanal, COUNT(*) AS total
-    FROM usuario_canais uc
-    JOIN canais c ON c.id = uc.fkCanais
-    WHERE uc.inscrito = true
+    FROM usuario_canais u_c
+    JOIN canais c ON c.id = u_c.fkCanais
+    WHERE u_c.inscrito = true
     GROUP BY c.nomeCanal;
   `;
   return database.executar(query);
 }
 
 module.exports = {
-  contarInscricoes,
+  contarInscricoes
 };
